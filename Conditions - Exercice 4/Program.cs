517bf33d0fb8362f7480c5e9302c4b1e781d86a6
SulyvanD <sulyvan.debraize@net-c.com>
2018-12-18 11:57:32 +0100

    Exercice completé

---
+++ Conditions - Exercice 4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditions___Exercice_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string login = "admin";
+            string password = "admin";
+            string user_login, user_password;
+
+            Console.WriteLine("Entrez votre identifiant");
+            user_login = Console.ReadLine();
+            Console.WriteLine("Entrez votre mot de passe");
+            user_password = Console.ReadLine();
+
+            if (user_login == login)
+            {
+                if (user_password == password)
+                {
+                    Console.WriteLine("Bienvenue à la Manu le Havre.");
+                }
+                else
+                {
+                    Console.WriteLine("Mauvais identifiant ou mauvais mot de passe.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Mauvais identifiant ou mauvais mot de passe.");
+            }
+        }
+    }
+}
